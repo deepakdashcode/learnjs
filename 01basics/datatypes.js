922166dc292a51a98cc27aa6026703d15901015b
deepakdashcode <dipudash.2003@gmail.com>
2025-05-15 20:34:06 +0530

    l5 done

---
+++ 01basics/datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js code as newer version
+
+// alert("Hello"). we cant execute it directly in node environment
+// console.log(3 + 3);
+// console.log("Deepak");
+
+
+let name = "Deepak"
+let age = 21
+let isLogged = false
+
+
+// number => 2 ^ 53
+// bigint
+// string
+// bool => false/true
+// null => standalone value
+// undefined => value not defined
+// symbol => uniqueness
+
+
+// object
+console.log(typeof "Deepak")
+console.log(typeof age);
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
---
+++ 01basics/variables.js
@@ -0,0 +1,7 @@
+const accountId = 10
+let accMail = "dk@gmail.com"
+var accPassword = "123"
+let state;
+console.table([accountId, accMail, accPassword, state])
+
+
